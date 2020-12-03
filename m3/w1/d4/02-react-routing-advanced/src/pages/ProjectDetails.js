9ff273006b3781eef22d3c30f8e0fcea4d64838f
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-03 16:39:26 +0100

    Update m3 d4

--- m3/w1/d4/02-react-routing-advanced/src/pages/ProjectDetails.js
+++ m3/w1/d4/02-react-routing-advanced/src/pages/ProjectDetails.js
@@ -5,21 +5,11 @@ import { projects } from './../data';
 
 function ProjectDetails(props) {
 
-  console.log('props', props)
-
   const projectId = props.match.params.projectId; // coming from the params (Navbar URL)
 
   const foundProject = projects.find( (project)=> {
     return project.id === projectId;
   }) 
-
-  /* 
-    id: '1a',
-    name: 'The Frogger Clone',
-    year: 2017,
-    technologies: 'JavaScript, jQuery',
-    description: 'The first project game clone.',
-  */
 
   return (
     <div>
@@ -27,7 +17,6 @@ function ProjectDetails(props) {
       <h2>{foundProject.name}</h2>
       <h3>Used Technologies: {foundProject.technologies}</h3>
       <p>{foundProject.description}</p>
-
 
     </div>
   )
