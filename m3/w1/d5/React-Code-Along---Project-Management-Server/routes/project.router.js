b0a4c21fd7d5ded0fdedd0a24cc7f202480da0c4
ross-u <cirkovic.uros.srb@gmail.com>
2020-12-04 13:27:27 +0100

    Add finished server/API example

--- m3/w1/d5/React-Code-Along---Project-Management-Server/routes/project.router.js
+++ m3/w1/d5/React-Code-Along---Project-Management-Server/routes/project.router.js
@@ -62,8 +62,43 @@ router.get('/projects/:id', (req, res, next) => {
 
 
 // PUT '/api/projects/:id' 		=> to update a specific project
+// PUT '/api/projects/:id' 		=> to update a specific project
+router.put('/projects/:id', (req, res, next)=>{
+  const { id } = req.params;
+  const { title, description } = req.body;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Project.findByIdAndUpdate(id, { title, description })
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 // DELETE '/api/projects/:id'   => to delete a specific project
+router.delete('/projects/:id', (req, res)=>{
+  const { id } = req.params;
 
+  if ( !mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Project.findByIdAndRemove(id)
+    .then(() => {
+      res
+        .status(202)  //  Accepted
+        .send(`Document ${id} was removed successfully.`);
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    })
+});
 
 module.exports = router;
